8ff4cdfe43262dd228bcd7db56531cbdece74e71
JulienSoft <juliendamn-eric@yahoo.fr>
2025-02-14 11:19:46 +0100

    Add cluster display

--- app/src/main/java/com/example/androidtest/ui/components/MapScreen.kt
+++ app/src/main/java/com/example/androidtest/ui/components/MapScreen.kt
@@ -4,26 +4,45 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.androidtest.viewmodel.OpenChargeMapViewModel
 import com.mapbox.geojson.Point
+import com.mapbox.maps.coroutine.mapLoadedEvents
+import com.mapbox.maps.dsl.cameraOptions
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
 import com.mapbox.maps.extension.compose.annotation.ViewAnnotation
+import com.mapbox.maps.extension.compose.annotation.generated.PointAnnotationGroup
 import com.mapbox.maps.extension.compose.rememberMapState
 import com.mapbox.maps.extension.compose.style.standard.LightPresetValue
 import com.mapbox.maps.extension.compose.style.standard.MapboxStandardStyle
+import com.mapbox.maps.extension.style.expressions.generated.Expression
+import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.literal
 import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.animation.MapAnimationOptions.Companion.mapAnimationOptions
+import com.mapbox.maps.plugin.annotation.AnnotationConfig
+import com.mapbox.maps.plugin.annotation.AnnotationSourceOptions
+import com.mapbox.maps.plugin.annotation.ClusterOptions
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
 import com.mapbox.maps.plugin.locationcomponent.location
 import com.mapbox.maps.plugin.viewport.data.FollowPuckViewportStateOptions
 import com.mapbox.maps.viewannotation.geometry
 import com.mapbox.maps.viewannotation.viewAnnotationOptions
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
@@ -90,19 +109,45 @@ fun MapScreen() {
                 // Change light mode with system
                 lightPreset = if (isDarkMode) LightPresetValue.NIGHT else LightPresetValue.DAY
             }
-        },
-    ) {
-        // Display annotations for each station on the map
-        stationsUIState.stations.filter { station -> station.isValid() }.map { station ->
-            ViewAnnotation(
-                options = viewAnnotationOptions {
-                    // View annotation is placed at the specific geo coordinate
-                    geometry(station.locationPoint())
-                    allowOverlap(true)
 
-                }
+            PointAnnotationGroup(
+                annotations = chargingStations.map {
+                    PointAnnotationOptions()
+                        .withPoint(it.locationPoint())
+                },
+                annotationConfig = AnnotationConfig(
+                    annotationSourceOptions = AnnotationSourceOptions(
+                        clusterOptions = ClusterOptions(
+                            textColorExpression = Expression.color(Color.Cyan.toArgb()),
+                            textSize = 20.0,
+                            clusterMaxZoom = 10,
+                            circleRadiusExpression = literal(20.0),
+                            colorLevels = listOf(
+                                Pair(100, Color.Red.toArgb()),
+                                Pair(50, Color.Blue.toArgb()),
+                                Pair(0, Color.Green.toArgb())
+                            )
+                        )
+                    )
+                ),
             ) {
-                MapChargingStationAnnotation(station)
+                interactionsState.isDraggable = true
+                interactionsState
+                    .onClusterClicked {
+                        // Fly in the cluster
+                        it.originalFeature.geometry()?.let { geometry ->
+                            mapViewportState.flyTo(
+                                cameraOptions {
+                                    center(geometry as Point)
+                                    zoom(12.0)
+                                },
+                                mapAnimationOptions {
+                                    duration(3_000)
+                                }
+                            )
+                        }
+                        true
+                    }
             }
         },
     ) {
